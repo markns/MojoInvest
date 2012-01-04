aa2d64fecf190ea1e0c4b222d864b3088acf8d95
Mark Nuttall-Smith <marknuttallsmith@gmail.com>
2012-01-04 23:13:51 +0530

    Copy local db script

---
+++ scripts/copy_local_db.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+
+cp ../data/local_db.bin \
+/Users/marknuttallsmith/Library/Caches/IntelliJIdea10/gwt/MojoInvest.4c511d6e/mojoinvest.217b5eb7/run/www/WEB-INF/appengine-generated/local_db.bin
