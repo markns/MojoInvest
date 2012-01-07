872ba8d49f6e0eb69449d2f6086a5c8e7a0f4c35
Mark Nuttall-Smith <marknuttallsmith@gmail.com>
2012-01-07 09:26:33 +0530

    TODOs for getperformancerangesavailable handler

--- src/main/java/com/mns/mojoinvest/server/handler/GetPerformanceRangesAvailableHandler.java
+++ src/main/java/com/mns/mojoinvest/server/handler/GetPerformanceRangesAvailableHandler.java
@@ -9,12 +9,14 @@ import com.mns.mojoinvest.shared.dispatch.GetPerformanceRangesAvailableResult;
 import java.util.Arrays;
 import java.util.List;
 
+//TODO: refactor to GetFormationPeriodsAvailableHandler
 public class GetPerformanceRangesAvailableHandler implements
         ActionHandler<GetPerformanceRangesAvailableAction, GetPerformanceRangesAvailableResult> {
 
     @Override
     public GetPerformanceRangesAvailableResult execute(GetPerformanceRangesAvailableAction action, ExecutionContext context) throws ActionException {
 
+        //TODO: Load formation periods available from database
         List<Integer> performanceRangeAvailable = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
 
         return new GetPerformanceRangesAvailableResult("", performanceRangeAvailable);
