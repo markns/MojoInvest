004001b5e65c3931127326d96d7d3df5ad984bad
Mark Nuttall-Smith <marknuttallsmith@gmail.com>
2012-01-07 09:35:27 +0530

    Reorganised app packages ... again

--- src/main/java/com/mns/mojoinvest/client/app/AppModule.java
+++ src/main/java/com/mns/mojoinvest/client/app/AppModule.java
@@ -1,7 +1,12 @@
 package com.mns.mojoinvest.client.app;
 
 import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
-import com.mns.mojoinvest.client.app.component.*;
+import com.mns.mojoinvest.client.app.chart.ChartPresenter;
+import com.mns.mojoinvest.client.app.chart.ChartView;
+import com.mns.mojoinvest.client.app.params.ParamsPresenter;
+import com.mns.mojoinvest.client.app.params.ParamsView;
+import com.mns.mojoinvest.client.app.trades.TradesPresenter;
+import com.mns.mojoinvest.client.app.trades.TradesView;
 
 public class AppModule extends AbstractPresenterModule {
 
