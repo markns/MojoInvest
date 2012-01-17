17fc4ee0462adea707ad4256d552cad6d44d9a16
Mark Nuttall-Smith <marknuttallsmith@gmail.com>
2012-01-17 11:05:45 +0530

    Upload fund config changes

--- scripts/upload_fund_data.sh
+++ scripts/upload_fund_data.sh
@@ -2,6 +2,9 @@
 
 echo ambersun | appcfg.py upload_data \
     --email=marknuttallsmith@gmail.com  \
-    --passin --application=mojoinvest \
-    --kind=Fund --filename=../data/funds.bin \
-    --url=http://localhost:8888/remote_api
+    --passin \
+	--config_file=config.yml \
+	--application=mojoinvest \
+    --kind=Fund \
+    --filename="../data/ishares_funds.csv" \
+    --url=http://localhost:8080/remote_api
