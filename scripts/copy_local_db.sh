98db57c3f94e900adbc1bcad5ef8ab15584909ea
Mark Nuttall-Smith <marknuttallsmith@gmail.com>
2012-03-08 10:25:24 +0530

    Updated to intellij 11 cache

--- scripts/copy_local_db.sh
+++ scripts/copy_local_db.sh
@@ -2,6 +2,6 @@
 
 
 cp ../data/local_db-ishares.bin \
-/Users/marknuttallsmith/Projects/MojoInvest/target/mojoinvest-0.1-SNAPSHOT/WEB-INF/appengine-generated/local_db.bin
-#/Users/marknuttallsmith/Library/Caches/IntelliJIdea10/gwt/MojoInvest.4c511d6e/mojoinvest.217b5eb7/run/www/WEB-INF/appengine-generated/local_db.bin
+/Users/marknuttallsmith/Library/Caches/IntelliJIdea11/gwt/MojoInvest.4c511d6e/mojoinvest.217b5eb7/run/www/WEB-INF/appengine-generated/local_db.bin
+#/Users/marknuttallsmith/Projects/MojoInvest/target/mojoinvest-0.1-SNAPSHOT/WEB-INF/appengine-generated/local_db.bin
 #/Users/marknuttallsmith/Library/Caches/IntelliJIdea10/gwt/MojoInvest.4c511d6e/mojoinvest.217b5eb7/run/www/WEB-INF/appengine-generated/local_db.bin
