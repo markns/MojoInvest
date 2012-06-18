4adde2d88a2bde62fdac57d1a0e6d7f2f877a98a
Mark Nuttall-Smith <marknuttallsmith@gmail.com>
2012-06-18 20:58:09 +0100

    Added additional resources and templates for about, help and terms pages

---
+++ src/main/java/com/mns/mojoinvest/server/resource/AboutResource.java
@@ -0,0 +1,22 @@
+package com.mns.mojoinvest.server.resource;
+
+import com.sun.jersey.api.view.Viewable;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.Map;
+
+@Path("/about")
+public class AboutResource {
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public Viewable getMytestView() {
+        Map<String, Object> map = new HashMap<String, Object>();
+        return new Viewable("/about.mustache", map);
+    }
+
+}
---
+++ src/main/java/com/mns/mojoinvest/server/resource/TermsResource.java
@@ -0,0 +1,21 @@
+package com.mns.mojoinvest.server.resource;
+
+import com.sun.jersey.api.view.Viewable;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.Map;
+
+@Path("/terms")
+public class TermsResource {
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public Viewable getMytestView() {
+        Map<String, Object> map = new HashMap<String, Object>();
+        return new Viewable("/terms.mustache", map);
+    }
+
+}
